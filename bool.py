56dadac19b5a3028c15edb790c8130dc8004310d
JaimeGaete2018 <39652295+JaimeGaete2018@users.noreply.github.com>
2018-05-26 14:13:21 -0400

    segundo commit

---
+++ bool.py
@@ -0,0 +1,15 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+import os, sys
+
+_H = "S"
+var11 = raw_input("Es hombre:[S/N]")
+
+if var11 == _H:
+    print("Es Hombre!")
+else:
+    print("Es Mujer!")
+
+    
+
+    
---
+++ companum.py
@@ -0,0 +1,12 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+import os, sys
+
+val = 6
+
+v2 = int(input("Ingrese numero:"))
+
+if v2==val:
+    print("iguales")
+else:
+    print("no igual")
