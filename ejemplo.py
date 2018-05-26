5df523f21e150f008d72edcb521a1ac0825c8a38
JaimeGaete2018 <39652295+JaimeGaete2018@users.noreply.github.com>
2018-05-26 12:50:45 -0400

    primer commit

---
+++ ejemplo.py
@@ -0,0 +1,11 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+import os, sys
+
+v1 = int(input("Ingrese el primer valor:"))
+v2 = int(input("Ingrese el segundo valor:"))
+v3 = int(input("Ingrese el tercer valor:"))
+v4 = int(input("Ingrese el cuarto valor:"))
+res = v1 + v2 + v3 + v4
+texto = "Resultado es: "
+print(texto, res)
